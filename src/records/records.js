32710c6e0438b42a37b84035bd294db34a2beb1a
Andri Baal <andri.baal@bluewin.ch>
2024-12-09 16:08:23 +0100

    Changed logo and distances

--- src/records/records.js
+++ src/records/records.js
@@ -5,8 +5,8 @@ export default [
         plz: 9000,
         street: 'Teufenerstrasse 11',
         preis: 5.0,
-        distance: 8,
-        img: './parkplatz1.jpg'
+        distance: 8000,
+        img: 'parkplatz1.jpg'
     },
     {
         name: 'Zentraler Parkplatz',
@@ -14,8 +14,8 @@ export default [
         plz: 9000,
         street: 'Marktplatz 1',
         preis: 7.0,
-        distance: 5,
-        img: './parkplatz2.jpg'
+        distance: 500,
+        img: 'parkplatz2.jpg'
     },
     {
         name: 'Parkplatz bei der Uni',
@@ -23,8 +23,8 @@ export default [
         plz: 9010,
         street: 'Dufourstrasse 50',
         preis: 6.5,
-        distance: 1,
-        img: './parkplatz3.jpg'
+        distance: 1000,
+        img: 'parkplatz3.jpg'
     },
     {
         name: 'Parkplatz in Abtwil',
@@ -33,7 +33,7 @@ export default [
         street: 'Hauptstrasse 25',
         preis: 4.0,
         distance: 2,
-        img: './parkplatz4.jpg'
+        img: 'parkplatz4.jpg'
     },
     {
         name: 'Bahnhof Parkplatz',
@@ -41,7 +41,7 @@ export default [
         plz: 9000,
         street: 'Bahnhofstrasse 12',
         preis: 8.5,
-        distance: 3,
-        img: './parkplatz5.jpg'
+        distance: 3000,
+        img: 'parkplatz5.jpg'
     }
 ]
